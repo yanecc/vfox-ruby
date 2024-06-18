0d8f5b228b5ca10bd530934e0cc8b115df95d6a8
Sunrise <yane@88.com>
2024-06-18 13:22:30 +0800

    Generate manifest body with UpdateVersionList

---
+++ .github/UpdateVersionList.cr
@@ -0,0 +1,80 @@
+require "http/client"
+require "json"
+require "option_parser"
+
+def getLatestVersion(url)
+  response = HTTP::Client.get url
+  latestRelease = response.headers["Location"]
+  latestVersion = latestRelease[/(\d[\.\d]+)/]
+end
+
+def fetchVersionList
+  response = HTTP::Client.get "https://github.com/yanecc/vfox-ruby/releases/manifest"
+  versionJson = response.body.match!(/<code>([\s\S]+)<\/code>/)[1]
+  versionList = Hash(String, Array(String)).from_json versionJson
+end
+
+def fetchJRubyVersions
+  response = HTTP::Client.get "https://www.jruby.org/files/downloads/index.html"
+  versionList = response.body.scan(/(9\.(1\.1[6-9]|2\.2\d|3\.1\d|[4-9]\.\d+)\.\d)<\/a>/).map { |m| m[1] }
+  versionList.reverse
+end
+
+def compareVersions(a, b)
+  a = a.split(".").map { |v| v.to_i }
+  b = b.split(".").map { |v| v.to_i }
+  a.each_with_index do |v, i|
+    if v != b[i]
+      return v <=> b[i]
+    end
+  end
+  0
+end
+
+latestTruffleRuby = getLatestVersion "https://github.com/oracle/truffleruby/releases/latest"
+latestHomebrewRuby = getLatestVersion "https://github.com/Homebrew/homebrew-portable-ruby/releases/latest"
+vlist = fetchVersionList
+
+output = "manifest.md"
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: #{Path[Process.executable_path.not_nil!].stem} [options]"
+  parser.on("-o PATH", "--output PATH", "Specify the output path") { |path| output = path }
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+isUpdated = true
+if vlist["ruby"][0] != latestHomebrewRuby
+  isUpdated = false
+  vlist["homebrew"].unshift(latestHomebrewRuby)
+end
+
+if vlist["jruby"] != fetchJRubyVersions
+  isUpdated = false
+  vlist["jruby"] = fetchJRubyVersions
+end
+
+if vlist["truffleruby"][0] != latestTruffleRuby
+  isUpdated = false
+  vlist["truffleruby"].unshift(latestTruffleRuby)
+  vlist["truffleruby-jvm"].unshift(latestTruffleRuby + ".jvm")
+end
+
+unless isUpdated
+  vlist["ruby"] = vlist["homebrew"] + vlist["conda-forge"]
+  vlist["ruby"].sort! { |a, b| compareVersions(b, a) }
+  vlist["ruby"] = vlist["ruby"] + vlist["truffleruby"] + vlist["truffleruby-jvm"]
+  File.open(output, "a") do |file|
+    file.puts "```"
+    file.puts vlist.to_pretty_json(indent = "    ")
+    file.puts "```"
+  end
+end
